0b9f61fe352c8ab5c3b6b41383063777234dd21c
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 16:35:59 +0530

    SizedBox Spacer Padding example

---
+++ android/app/src/main/kotlin/com/alaa/sizedboxspacerpadding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alaa.sizedboxspacerpadding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:sizedboxspacerpadding/my_home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'SizedBox & Padding & Spacer widgets Example',
+    home: MyHome(),
+  ));
+}
